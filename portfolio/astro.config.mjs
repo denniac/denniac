b484a9cc517b8782253bc3c2f1e3d8b6a174585c
Denilson Anachury <denilson.anachury@appgate.com>
2026-05-15 15:27:46 -0500

    📈 feat: integrate Vercel Analytics and update dependencies

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -5,7 +5,11 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    }
+  }),
 
   vite: {
     plugins: [tailwindcss()],
